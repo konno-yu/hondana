98fce2fcef0d9c413793bb308ee88bf002dfbac9
konno-yu <yuta.konno.socie@gmail.com>
2023-05-04 22:50:34 +0900

    ヘッダ実装（最低限）

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,21 @@
-import { InputTodoForm } from "/components/InputTodoForm";
-import { TodoList } from "/components/TodoList";
-
+import { css } from '@emotion/react';
+import { Header } from '/components/Header';
+import { InputTodoForm } from '/components/InputTodoForm';
+import { TodoList } from '/components/TodoList';
 
 const Home = () => {
   return (
-    <>
+    <div css={rootStyle}>
+      <Header />
       <InputTodoForm />
-      <TodoList/>
-    </>
-  )
-}
+      <TodoList />
+    </div>
+  );
+};
+
+const rootStyle = css`
+  height: 100vh;
+  /* background: gray; */
+`;
 
 export default Home;
