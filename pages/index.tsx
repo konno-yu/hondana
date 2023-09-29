2a37cdb1acb7cacff868770654df89883cfa54d6
konno-yu <yuta.konno.socie@gmail.com>
2023-09-30 00:18:56 +0900

    ヘッダを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,39 @@ import { css } from '@emotion/react';
 import BookDrawer from '../components/BookDrawer';
 import SectionDrawer from '/components/SectionDrawer';
 import AwarenessDrawer from '/components/AwarenessDrawer';
+import { Major_Mono_Display } from 'next/font/google';
+
+const MMD400 = Major_Mono_Display({
+  weight: '400',
+  preload: false,
+});
 
 const Home = () => {
   return (
     <div css={rootStyle}>
-      <BookDrawer />
-      <SectionDrawer />
-      <AwarenessDrawer />
+      <div
+        className={MMD400.className}
+        style={{
+          fontSize: 24,
+          height: '5%',
+          width: '100%',
+          background: '#eceff1',
+          borderBottom: '0.5px solid #DEDEDE',
+          display: 'flex',
+          alignItems: 'flex-start',
+          padding: '4px 8px',
+          fontWeight: 900,
+          color: '#546e7a',
+          letterSpacing: 1.5,
+        }}
+      >
+        HONDANA
+      </div>
+      <div style={{ display: 'flex', height: '100%', width: '100%' }}>
+        <BookDrawer />
+        <SectionDrawer />
+        <AwarenessDrawer />
+      </div>
     </div>
   );
 };
@@ -17,6 +43,7 @@ const rootStyle = css`
   height: 100vh;
   width: 100%;
   display: flex;
+  flex-direction: column;
 `;
 
 export default Home;
