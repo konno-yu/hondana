2cdecee2cea87ab2a293ef9dd29eabed84aa1368
konno-yu <yuta.konno.socie@gmail.com>
2023-10-12 00:25:12 +0900

    本の追加・編集ダイアログを実装

--- components/BookDrawer.tsx
+++ components/BookDrawer.tsx
@@ -12,6 +12,7 @@ const BookDrawer = () => {
   const [author, setAuthor] = useState('');
   const [rate, setRate] = useState(0);
   const { books, postBooks, fetchBooks, putBooks, deleteBooks } = useBooks();
+  const [isOpen, setIsOpen] = useState(false);
 
   const onChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(event.target.value);
@@ -26,10 +27,19 @@ const BookDrawer = () => {
     setRate(event.target.value as unknown as number);
   };
 
-  const openDlg = () => {
-    if (dialogRef.current) {
-      dialogRef.current.showModal();
+  useEffect(() => {
+    if (isOpen && !dialogRef.current?.open) {
+      dialogRef.current?.showModal();
+    } else if (!isOpen && dialogRef.current?.open) {
+      dialogRef.current?.close();
     }
+  }, [isOpen]);
+
+  const openDlg = () => {
+    // if (dialogRef.current) {
+    setIsOpen(true);
+    //   dialogRef.current.showModal();
+    // }
   };
 
   const resetInput = () => {
@@ -40,10 +50,11 @@ const BookDrawer = () => {
   };
 
   const closeDlg = () => {
-    if (dialogRef.current) {
-      resetInput();
-      dialogRef.current.close();
-    }
+    // if (dialogRef.current) {
+    resetInput();
+    setIsOpen(false);
+    //   dialogRef.current.close();
+    // }
   };
 
   const closeDlgWithSave = () => {
@@ -72,6 +83,7 @@ const BookDrawer = () => {
         }
       });
     }
+    setIsOpen(false);
   };
 
   const handleEdit = (id) => {
@@ -117,73 +129,84 @@ const BookDrawer = () => {
           />
         ))}
       </div>
-      <dialog css={dialog} ref={dialogRef}>
-        <div style={{ textAlign: 'center', height: '10%', fontSize: '22pt', fontWeight: 700 }}>Add Books</div>
+      {isOpen && (
+        <dialog css={dialog} ref={dialogRef}>
+          <div style={{ textAlign: 'center', height: '10%', fontSize: '22pt', fontWeight: 700 }}>Add Books</div>
 
-        <div
-          style={{
-            height: '70%',
-            fontSize: '16pt',
-            fontWeight: 700,
-            padding: 16,
-            display: 'flex',
-            justifyContent: 'space-evenly',
-            flexDirection: 'column',
-          }}
-        >
-          <div style={{ display: 'flex', gap: 8, flexDirection: 'row' }}>
-            <span style={{ width: '20%' }}>Title</span>
-            <Input width={350} value={title} onChange={onChangeTitle} />
-          </div>
-          <div style={{ display: 'flex', gap: 8, flexDirection: 'row' }}>
-            <span style={{ width: '20%' }}>Author</span>
-            <Input width={350} value={author} onChange={onChangeAuthor} />
-          </div>
-          <div style={{ display: 'flex', gap: 16, flexDirection: 'row' }}>
-            <span style={{ width: '20%' }}>Useful</span>
-            <select value={rate} onChange={onChangeRate} style={{ border: 'none', width: 200, fontWeight: 600, color: '#333' }}>
-              <option value="0">☆☆☆ - 評価前</option>
-              <option value="1">★☆☆ - もう読まない</option>
-              <option value="2">★★☆ - 時間があれば読む</option>
-              <option value="3">★★★ - また読みたい</option>
-            </select>
-          </div>
-        </div>
-        <div
-          style={{
-            height: '15%',
-            fontSize: '22pt',
-            fontWeight: 700,
-            display: 'flex',
-            gap: 24,
-            alignItems: 'end',
-            justifyContent: 'center',
-          }}
-        >
-          <button
-            onClick={closeDlgWithSave}
+          <div
             style={{
-              background: '#333333',
-              color: '#FFF',
-              border: 'none',
-              borderRadius: 4,
-              height: '80%',
-              width: '20%',
-              fontSize: 18,
+              height: '70%',
+              fontSize: '16pt',
               fontWeight: 700,
-              cursor: 'pointer',
+              padding: 16,
+              display: 'flex',
+              justifyContent: 'space-evenly',
+              flexDirection: 'column',
             }}
           >
-            Save
-          </button>
-          <button
-            style={{ background: '#FFF', border: 'none', borderRadius: 4, height: '80%', width: '20%', fontSize: 18, fontWeight: 700 }}
-            onClick={closeDlg}
+            <div style={{ display: 'flex', gap: 8, flexDirection: 'row' }}>
+              <span style={{ width: '10%' }}>Title</span>
+              <Input width={900} value={title} onChange={onChangeTitle} />
+            </div>
+            <div style={{ display: 'flex', gap: 8, flexDirection: 'row' }}>
+              <span style={{ width: '10%' }}>Author</span>
+              <Input width={900} value={author} onChange={onChangeAuthor} />
+            </div>
+            <div style={{ display: 'flex', gap: 16, flexDirection: 'row' }}>
+              <span style={{ width: '10%' }}>Useful</span>
+              <select value={rate} onChange={onChangeRate} style={{ border: 'none', width: 200, fontWeight: 600, color: '#333' }}>
+                <option value="0">☆☆☆ - 評価前</option>
+                <option value="1">★☆☆ - もう読まない</option>
+                <option value="2">★★☆ - 時間があれば読む</option>
+                <option value="3">★★★ - また読みたい</option>
+              </select>
+            </div>
+          </div>
+          <div
+            style={{
+              height: '15%',
+              fontSize: '22pt',
+              fontWeight: 700,
+              display: 'flex',
+              gap: 24,
+              alignItems: 'end',
+              justifyContent: 'center',
+            }}
           >
-            Cancel
-          </button>
-        </div>
-      </dialog>
+            <button
+              onClick={closeDlgWithSave}
+              style={{
+                background: '#333333',
+                color: '#FFF',
+                border: 'none',
+                borderRadius: 4,
+                height: '80%',
+                width: '10%',
+                fontSize: 18,
+                fontWeight: 700,
+                cursor: 'pointer',
+              }}
+            >
+              Save
+            </button>
+            <button
+              style={{
+                background: '#FFF',
+                border: 'none',
+                borderRadius: 4,
+                height: '80%',
+                width: '10%',
+                fontSize: 18,
+                fontWeight: 700,
+                cursor: 'pointer',
+              }}
+              onClick={closeDlg}
+            >
+              Cancel
+            </button>
+          </div>
+        </dialog>
+      )}
     </div>
   );
 };
@@ -206,13 +229,16 @@ const header = css`
 `;
 
 const dialog = css`
-  width: 40%;
+  width: 80%;
   height: 60%;
   border: none;
   border-radius: 16px;
   padding: 24px 32px;
   position: absolute;
   margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   &::backdrop {
     background: #666666;
     opacity: 0.5;
